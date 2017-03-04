c5d6fad1616fda6985c7e8cf1f31200dcc7aae80
Yannik <y.seyberth@gmail.com>
2017-03-04 20:24:19 +0100

    Converted project to kotlin

---
+++ app/src/main/java/de/ys_solutions/magic_thegathering/MagicActivity.kt
@@ -0,0 +1,117 @@
+package de.ys_solutions.magic_thegathering
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.ActionBar
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class MagicActivity : AppCompatActivity() {
+    private val mHideHandler = Handler()
+    private var mContentView: View? = null
+    private val mHidePart2Runnable = Runnable {
+        // Delayed removal of status and navigation bar
+
+        // Note that some of these constants are new as of API 16 (Jelly Bean)
+        // and API 19 (KitKat). It is safe to use them, as they are inlined
+        // at compile-time and do nothing on earlier devices.
+        mContentView!!.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
+    private var mVisible: Boolean = false
+    private val mHideRunnable = Runnable { hide() }
+
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_fullscreen)
+
+        mVisible = true
+        mContentView = findViewById(R.id.fullscreen_content)
+
+
+        // Set up the user interaction to manually show or hide the system UI.
+        mContentView!!.setOnClickListener {
+            //toggle();
+        }
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100)
+    }
+
+    private fun toggle() {
+        if (mVisible) {
+            hide()
+        } else {
+            show()
+        }
+    }
+
+    private fun hide() {
+        // Hide UI first
+        supportActionBar?.hide()
+        mVisible = false
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun show() {
+        // Show the system bar
+        mContentView!!.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        mVisible = true
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable)
+    }
+
+    /**
+     * Schedules a call to hide() in [delayMillis] milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private fun delayedHide(delayMillis: Int) {
+        mHideHandler.removeCallbacks(mHideRunnable)
+        mHideHandler.postDelayed(mHideRunnable, delayMillis.toLong())
+    }
+
+    companion object {
+        /**
+         * Whether or not the system UI should be auto-hidden after
+         * [.AUTO_HIDE_DELAY_MILLIS] milliseconds.
+         */
+        private val AUTO_HIDE = true
+
+        /**
+         * If [.AUTO_HIDE] is set, the number of milliseconds to wait after
+         * user interaction before hiding the system UI.
+         */
+        private val AUTO_HIDE_DELAY_MILLIS = 1000
+
+        /**
+         * Some older devices needs a small delay between UI widget updates
+         * and a change of the status and navigation bar.
+         */
+        private val UI_ANIMATION_DELAY = 300
+    }
+}
